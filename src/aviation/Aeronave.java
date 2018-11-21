6070df3f9d8be5ea07535aedb2e78eb42eb7bf92
Rafael Magalhaes <rafaelmagftw@gmail.com>
2018-11-21 15:02:00 +0000

    added take off protocol to Aeronave.

--- src/aviation/Aeronave.java
+++ src/aviation/Aeronave.java
@@ -1,11 +1,13 @@
 package aviation;
 
+import java.awt.PageAttributes.OriginType;
 import java.util.ArrayList;
 import java.util.Random;
 
 import jade.core.*;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -17,25 +19,26 @@ import jade.lang.acl.MessageTemplate;
 public class Aeronave extends Agent{
 	
 	private Map mapa;
+	private int estado; // 0 -> para descolar, 1-> voo, 2-> para aterrar
 	private AID destino;
 	private AID aeroportoAtual;
 	private int alertZone, protectedZone, nPassageiros, distPercorrida, velocidade, distPrevista;
-	private ArrayList<Integer> condMeteo;
+	//private ArrayList<Integer> condMeteo;
 	private Position posicao;
 	private Position pos_destino;
 	private ArrayList<AID> aeroportos, aeronaves;
-	private Position[] rota;
+	private ArrayList<Position> rota;
 	private long tempoPartida, tempoVoo;
-	
-	
 	
 	@Override
 	protected void setup() {
 		mapa = new Map();
+		rota = new ArrayList<Position>();
 		aeroportos = new ArrayList<AID>();
 		aeronaves = new ArrayList<AID>();
 		Random rand = new Random();
 		nPassageiros = rand.nextInt(50)+50;
+		estado = 0;
 		
 		
 		DFAgentDescription dfd = new DFAgentDescription();
@@ -50,25 +53,27 @@ public class Aeronave extends Agent{
             
             DFService.register(this, dfd);
             
+            this.addBehaviour(new ProcessAirportReplies());
             //Fetch all the airports
             this.addBehaviour(new FetchAirportsBehav());
             //Pick origin and destination and their coordinates
-            
+            this.addBehaviour(new PickOriginDestinyBehav());
             //Fill map with airports and territories (*)
-            
+            this.addBehaviour(new RequestAirportPositions());
             //Calculate route
-            
-            //Ask for permission
-            
-            
+            //Ask for permission to take off
+           
             
         } catch (FIPAException e) {
             e.printStackTrace();
         }
 	}
 	
+	/**
+	 * Fetch all the airports registered in the DF service and store their AID.
+	 *
+	 */
 	class FetchAirportsBehav extends SimpleBehaviour{
-		// Fetch all the airports registered in the DF service and store their AID.
 
 		@Override
 		public void action() {
@@ -105,64 +110,237 @@ public class Aeronave extends Agent{
 		
 	}
 	
-	class PickADBehav extends OneShotBehaviour{
-		// Find the origin and destination airports.
-
+	/**
+	 *  Find the origin and destination airports.
+	 *	This behavior may be called n times if the origin airport can't receive the airplane.
+	 *  Randomly picks 2 indexes and assigns them to the actual and the destination airports
+	 *
+	 */
+	class PickOriginDestinyBehav extends SimpleBehaviour{
+		
+		boolean onlyOrigin, found;
+		
+		public PickOriginDestinyBehav() {
+			onlyOrigin = false;
+			found = false;
+		}
+		
 		@Override
 		public void action() {
 			Random rand = new Random();
-			// Randomly picks 2 indexes and assigns them to the actual and the destination airports
-			while(true) {
-				int o = rand.nextInt(aeroportos.size());
-				int d = rand.nextInt(aeroportos.size());
-				if(o != d) {
-					aeroportoAtual = aeroportos.get(o);
-					destino = aeroportos.get(d);
-					break;
+			
+			do {
+				aeroportoAtual = aeroportos.get(rand.nextInt(aeroportos.size()));
+				if(!onlyOrigin) {
+					destino = aeroportos.get(rand.nextInt(aeroportos.size()));
+					onlyOrigin = true;
+				}
+			} while(aeroportoAtual != destino);
+			
+			//Send the request to the picked origin airport
+			ACLMessage request = new ACLMessage(ACLMessage.PROPOSE);
+			request.setOntology("propose-birth");
+			request.setConversationId(""+ System.currentTimeMillis());
+			request.addReceiver(aeroportoAtual);
+			myAgent.send(request);
+			
+			//Wait for the request
+			MessageTemplate mt1 = MessageTemplate.MatchPerformative(ACLMessage.PROPOSE);
+			MessageTemplate mt2 = MessageTemplate.MatchOntology("propose-birth");
+			MessageTemplate mt3 = MessageTemplate.and(mt1, mt2);
+			
+			ACLMessage reply = blockingReceive(mt3);
+			if(reply != null) {
+				if(reply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+					// If it proposal is accepted
+					AID airport = reply.getSender();
+					String[] content_split = reply.getContent().split("::");
+					int x = Integer.parseInt(content_split[0]);
+					int y = Integer.parseInt(content_split[1]);
+					Position pos = new Position(x,y);
+					mapa.setAirport(pos, airport, false);
+					posicao = pos;
+					found = true;
+				}
+				// else found still's false
+			}
+		}
+
+		@Override
+		public boolean done() {
+			return found;
+		}
+	}
+	
+	/**
+	 * Send a request to all airports except origin and destiny for their position
+	 *
+	 */
+	class RequestAirportPositions extends OneShotBehaviour{
+		
+		@Override
+		public void action() {
+			for(AID aero: aeroportos) {
+				if(aero != aeroportoAtual) {
+					ACLMessage request_info = new ACLMessage(ACLMessage.REQUEST);
+					request_info.setConversationId(""+System.currentTimeMillis());
+					request_info.setOntology("request-pos");
+					request_info.addReceiver(aero);
+					myAgent.send(request_info);
 				}
 			}
 		}
 	}
 	
-	class AirportRequestBehav extends OneShotBehaviour{
-		// Send a request to all airports for their position
-		// Can be a request for info or for meteo
-		String ontology;
-		public AirportRequestBehav(String ontology) {
-			this.ontology = ontology;
-		}
-
+	class AskForTakeOffBehav extends OneShotBehaviour{
 		@Override
 		public void action() {
-			for(AID aero: aeroportos) {
-				// Request the position of the airport
-				ACLMessage request_info = new ACLMessage(ACLMessage.REQUEST);
-				request_info.setConversationId(""+System.currentTimeMillis());
-				request_info.setOntology(ontology);
-				request_info.addReceiver(aero);
-				myAgent.send(request_info);
-			}
+			ACLMessage request = new ACLMessage(ACLMessage.PROPOSE);
+			request.setOntology("propose-takeoff");
+			request.setConversationId(""+ System.currentTimeMillis());
+			request.addReceiver(aeroportoAtual);
+			myAgent.send(request);
 		}
 	}
 	
-	class ProcessAirportReplies extends CyclicBehaviour{
-		// Process the replies of the airports to info and meteo updates and to proposes!
+	class CalculateRouteBehav extends OneShotBehaviour{
+		
+		@Override
+		public void action() {
+			rota = mapa.calculateRoute(posicao, pos_destino);
+		}
+		
+	}
+	
+	/**
+	 *  Initiate the takeoff of the airplane
+	 *
+	 */
+	class StartTakeOffBehav extends SimpleBehaviour{
+		 private boolean takedoff = false;
+		 
+		public StartTakeOffBehav(Agent a) {
+			super(a);
+			takedoff = false;
+		}
 
 		@Override
 		public void action() {
-			MessageTemplate mt1 = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
-			MessageTemplate mt2 = MessageTemplate.MatchOntology("request-info");
-			MessageTemplate mt3 = MessageTemplate.MatchOntology("request-meteo");
-			MessageTemplate mt4 = MessageTemplate.or(mt2,mt3);
-			MessageTemplate mt5 = MessageTemplate.and(mt1,mt4);
-			
-			ACLMessage reply = receive(mt5);
-			
-			/*Stopped here because i didn't knew where i was going.*/
+			if(rota.size() > 0) {
+				tempoPartida = System.currentTimeMillis();
+				estado = 1;
+				posicao = rota.get(0);
+				rota.remove(0);
+				
+				
+				ACLMessage request = new ACLMessage(ACLMessage.PROPOSE);
+				request.setOntology("info-takeoff");
+				request.setConversationId(""+ System.currentTimeMillis());
+				request.addReceiver(aeroportoAtual);
+				myAgent.send(request);
+				takedoff = true;
+				
+				//send message to interface too
+				
+				System.out.println(""+myAgent.getLocalName()+" as taken flight!");
+			}
+		}
+
+		@Override
+		public boolean done() {
+			return takedoff;
+		}
+		
+		
+	}
+	
+	class StartLandingBehav extends OneShotBehaviour{
+
+		@Override
+		public void action() {
 			
 			
 		}
 		
+	}
+	
+	class WaitLandingBehav extends CyclicBehaviour{
+		@Override
+		public void action() {
+			
+		}
+	}
+	
+	/**
+	 * Process the replies of the airports and take specific actions.
+	 *
+	 */
+	class ProcessAirportReplies extends CyclicBehaviour{
 		
+		private int nr_airports_inserted = 0;
+
+		@Override
+		public void action() {
+			
+			ACLMessage reply = receive();
+			
+			if(reply != null) {
+				String ontology = reply.getOntology();
+				if(ontology == "request-pos") {
+					
+					//Process airport position request
+					AID airport = reply.getSender();
+					String[] content_split = reply.getContent().split("::");
+					int x = Integer.parseInt(content_split[0]);
+					int y = Integer.parseInt(content_split[1]);
+					Position pos = new Position(x,y);
+					if(airport == destino) {
+						mapa.setAirport(pos, airport, false);
+						pos_destino = pos;
+					}else
+						mapa.setAirport(pos, airport, true);
+					
+					nr_airports_inserted++;
+					// If all the airports have reported then ask for take-off and start finding route
+					if(nr_airports_inserted == aeroportos.size()-1) {
+						myAgent.addBehaviour(new AskForTakeOffBehav());
+						myAgent.addBehaviour(new CalculateRouteBehav());
+					}
+					
+				} else if(ontology == "propose-takeoff") {
+					
+					int perf = reply.getPerformative();
+					if(perf == ACLMessage.ACCEPT_PROPOSAL) {
+						// If it has received order to take-off and route is calculated then start take-off
+						if(rota.size() > 0)
+							myAgent.addBehaviour(new StartTakeOffBehav(myAgent));
+					}
+					
+				} else if(ontology == "propose-land"){
+					
+					int perf = reply.getPerformative();
+					if(perf == ACLMessage.ACCEPT_PROPOSAL) {
+						myAgent.addBehaviour(new StartLandingBehav());
+					} else {
+						myAgent.addBehaviour(new WaitLandingBehav());
+					}
+					
+				} else if(ontology == "info-meteo"){
+					//Process airport meteorology request
+					AID airport = reply.getSender();
+					String[] content = reply.getContent().split("::");
+					
+					int meteo = Integer.parseInt(content[2]);
+					if(meteo == 3) {
+						int x = Integer.parseInt(content[1]);
+						int y = Integer.parseInt(content[1]);
+						Position pos = new Position(x,y);
+						mapa.setBadWeather(pos);
+					}
+				}
+			} else {
+				block();
+			}
+		}
 	}
 }
--- src/aviation/Map.java
+++ src/aviation/Map.java
@@ -1,20 +1,22 @@
 package aviation;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jade.core.AID;
 
 public class Map {
 	
-	private int[][] map;
-	private AID[][] territory;
 	private final int TERRI_LENGTH = 200;
 	private final int RESTRICTED_ZONE = 60;
-	private final int DESTINY = 3;
-	private final int ORIGIN = 2;
+	private final int MAP_SIZE = 1000;
+	private int[][] map;
+	private AID[][] territory;
 	
 	public Map() {
-		map = new int[1000][1000];
-		territory = new AID[1000][1000];
+		map = new int[MAP_SIZE][MAP_SIZE];
+		territory = new AID[MAP_SIZE][MAP_SIZE];
 		initMap();
 	}
 	
@@ -26,25 +28,55 @@ public class Map {
 		}
 	}
 	
-	public void setDestiny(Position pos) {
-		map[pos.getX()][pos.getY()] = DESTINY;
-	}
-	public void setOrigin(Position pos) {
-		map[pos.getX()][pos.getY()] = ORIGIN;
+	public ArrayList<Position> calculateRoute(Position atual_pos, Position dest_pos) {
+		Node initialNode = new Node(atual_pos.getX(), atual_pos.getY());
+        Node finalNode = new Node(dest_pos.getX(), dest_pos.getY());
+        int rows = MAP_SIZE, cols = MAP_SIZE;
+        AStar aStar = new AStar(rows, cols, map, initialNode, finalNode);
+       
+        List<Node> path = aStar.findPath();
+        ArrayList<Position> pos = new ArrayList<Position>();
+        for (Node node : path) {
+            Position p = new Position(node.getRow(), node.getCol());
+            pos.add(p);
+        }
+        return pos;
 	}
 	
 	private void initMap() {
-		for(int i = 0; i < 1000; i++)
-			for(int j = 0; j < 1000; j++)
+		for(int i = 0; i < MAP_SIZE; i++)
+			for(int j = 0; j < MAP_SIZE; j++)
 				map[i][j] = 0;
 	}
-	
+	/**
+	 * Set the territory to an AID
+	 * @param pos
+	 * @param id
+	 */
 	private void setTerritory(Position pos, AID id) {
 		int x = pos.getX()-(TERRI_LENGTH/2), y = pos.getY()-(TERRI_LENGTH/2);
 		for(int i = 0; i < TERRI_LENGTH; i++) {
 			for(int j = 0; j < TERRI_LENGTH; j++) {
-				if(x+i >= 0 && y+j >= 0 && x+i < 1000 && y+j < 1000)
+				if(x+i >= 0 && y+j >= 0 && x+i < MAP_SIZE && y+j < MAP_SIZE)
 					territory[x+i][y+j]= id;
+			}
+		}
+	}
+	
+	private AID getTerritory(Position pos) {
+		return  territory[pos.getX()][pos.getY()];
+	}
+	
+	/**
+	 * Change the corresponding territory to restricted.
+	 * @param pos
+	 */
+	public void setBadWeather(Position pos) {
+		int x = pos.getX()-(TERRI_LENGTH/2), y = pos.getY()-(TERRI_LENGTH/2);
+		for(int i = 0; i < TERRI_LENGTH; i++) {
+			for(int j = 0; j < TERRI_LENGTH; j++) {
+				if(x+i >= 0 && y+j >= 0 && x+i < MAP_SIZE && y+j < MAP_SIZE)
+					map[x+i][y+j] = 1;
 			}
 		}
 	}
@@ -53,7 +85,7 @@ public class Map {
 		int x = pos.getX()-(RESTRICTED_ZONE/2), y = pos.getY()-(RESTRICTED_ZONE/2);
 		for(int i = 0; i < RESTRICTED_ZONE; i++) {
 			for(int j = 0; j < RESTRICTED_ZONE; j++) {
-				if(x+i >= 0 && y+i >= 0 && x+1 < 1000 && y+1 < 1000)
+				if(x+i >= 0 && y+i >= 0 && x+1 < MAP_SIZE && y+1 < MAP_SIZE)
 					map[x+i][y+i]= 1;
 			}
 		}
